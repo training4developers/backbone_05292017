a3a2a33ecdca27d16c4e2870a5bf4f0c9245e13c
Eric W. Greene <eric@ericwgreene.com>
2017-05-30 10:04:01 -0600

    added code demos

--- demos/js/index.js
+++ demos/js/index.js
@@ -13,11 +13,22 @@ var Person = Backbone.Model.extend({
 
 });
 
-var person = new Person({
+var person1 = new Person({
     firstName: 'Justin',
     lastName: 'Trudeau',
     age: 45
 });
 
-console.log(person.getFullName());
+var person2 = new Person({
+    firstName: 'Andrew',
+    lastName: 'Scheer',
+    age: 38
+});
 
+console.log(person1 === person2);
+console.log(person1.getFullName === person2.getFullName);
+console.log(Object.getPrototypeOf(person1) === Object.getPrototypeOf(person2));
+console.dir(person1);
+console.dir(person2);
+console.log(person1.getFullName());
+console.log(person2.getFullName());
